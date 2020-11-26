e352f346659ac5cd8ebd4c77274d0e3d99546334
Huu Lane <huulane@gmail.com>
2020-11-26 15:27:09 +0800

    feat: fake data generator

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/HuuLane/just-lil-random-logger
 
 go 1.15
+
+require github.com/bxcodec/faker/v3 v3.5.0
--- utils.go
+++ utils.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"math/rand"
 	"os"
 	"time"
+
+	"github.com/bxcodec/faker/v3"
 )
 
 func openFileAppendly(filename string) (io.Writer, func()) {
@@ -42,4 +45,35 @@ func randomTimerBetween(a, b int) chan void {
 		loop()
 	}()
 	return ch
+}
+
+type FakeUser struct {
+	UUID string `json:"uuid" faker:"uuid_digit"`
+	Name string `json:"name" faker:"name"`
+
+	PhoneNumber string `json:"phone_number" faker:"phone_number"`
+	Email       string `faker:"email" json:"email"`
+
+	Latitude  float32 `faker:"lat" json:"latitude"`
+	Longitude float32 `faker:"long" json:"longitude"`
+
+	DomainName string `faker:"domain_name" json:"domain_name"`
+	IPV4       string `faker:"ipv4" json:"ipv4"`
+	IPV6       string `faker:"ipv6" json:"ipv6"`
+
+	CreditCardNumber string `faker:"cc_number" json:"cc_number"`
+	CreditCardType   string `faker:"cc_type" json:"cc_type"`
+	PaymentMethod    string `faker:"oneof: cc, paypal, check, money order" json:"payment_method"` // oneof will randomly pick one of the comma-separated values supplied in the tag
+
+	Comment string `faker:"sentence" json:"comment"`
+}
+
+func FakeData() string {
+	a := FakeUser{}
+	err := faker.FakeData(&a)
+	if err != nil {
+		panic(err)
+	}
+	d, _ := json.Marshal(a)
+	return string(d)
 }
